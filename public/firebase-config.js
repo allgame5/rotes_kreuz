8c4a375eb69a770422d969b415a07aad724fad1b
Loes <all.game.11king@gmail.com>
2025-06-15 07:33:04 +0200

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -3,12 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAhj_c8zrE36vIJhdKfaqJ1q4eHAtE988k",
+  authDomain: "rotes-kreuz-55946.firebaseapp.com",
+  projectId: "rotes-kreuz-55946",
+  storageBucket: "rotes-kreuz-55946.firebasestorage.app",
+  messagingSenderId: "598428252142",
+  appId: "1:598428252142:web:778af9eb1dcc2c303240ff",
+  measurementId: "G-3T939H2848"
 };
 
 const app = initializeApp(firebaseConfig);
